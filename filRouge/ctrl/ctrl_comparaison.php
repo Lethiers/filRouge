a8f799a26894abb165745afcc6965351e1eb66e7
Lethiers <fabien.tarne@gmail.com>
2022-05-31 22:46:31 +0200

    chart js fonctionne

--- filRouge/ctrl/ctrl_comparaison.php
+++ filRouge/ctrl/ctrl_comparaison.php
@@ -13,15 +13,15 @@ include './view/view_comparaison.php';
 // -- logical
 
 // choix du prevision
-$prevision = new prevision();
-$tabloprevision = $prevision->showprevisionById($bdd,$_SESSION['id']);
+// $prevision = new prevision();
+// $tabloprevision = $prevision->showprevisionById($bdd,$_SESSION['id']);
 
-echo '<select name="prevision" class="bouton">
-<option value="">choisir une prévision</option>';
-foreach($tabloprevision as $value){
-    echo '<option value='.$value->id_prevision.'>'.$value->nom_prevision.'</option>';
-}
-echo '</select>';
+// echo '<select name="prevision" class="bouton">
+// <option value="">choisir une prévision</option>';
+// foreach($tabloprevision as $value){
+//     echo '<option value='.$value->id_prevision.'>'.$value->nom_prevision.'</option>';
+// }
+// echo '</select>';
 echo '<input type="submit" value="comparer" class="bouton">';
 echo '</form>';
 
@@ -43,76 +43,99 @@ echo'
 echo '</div>';
 
 
+$operation = new Operation ();
+
+$tabloOperation = $operation->showAllOperationByDate($bdd,$_POST['date_operation'],$_SESSION['id']);
+var_dump($tabloOperation);
+
+
+$tabloNom=[];
+$tabloMontant=[];
+
+foreach($tabloOperation as $value){
+    if ($value->nom_operation != null) {
+        array_push($tabloNom, $value->nom_operation);
+    }
+    if ($value->montant_operation != null) {
+        array_push($tabloMontant, $value->montant_operation);
+    }
+}
+
+// je convertis les données au format json pour utiliser les datas avec chartJs
+$tabloNomJson = json_encode($tabloNom);
+$tabloMontantJson = json_encode($tabloMontant);
+
+
 ///////////////////////////////////// EN COURS  //////////////////////////////////
 // reussir a afficher les operations selon un selecteur
 // choix operation
 
-$tabloOpTotalPos = [];
-$tabloOpTotalNeg = [];
+// $tabloOpTotalPos = [];
+// $tabloOpTotalNeg = [];
 
-$operation = new Operation();
-$tabloOperation = $operation->showAllOperationByIdUtil($bdd,$_SESSION['id']);
-// var_dump($tabloOperation);
-foreach($tabloOperation as $value){
-    if ($value->montant_operation > 0) {
-        array_push($tabloOpTotalPos,$value->montant_operation);
-    }
-    if ($value->montant_operation < 0) {
-        array_push($tabloOpTotalNeg,$value->montant_operation);
-    }
-}
-$operationPos = array_sum($tabloOpTotalPos);
-$operationNeg = array_sum($tabloOpTotalNeg);
-// echo 'vous avez gagné '.$operationPos.'€ et dépensé '.$operationNeg.'€';
-// j'arrive à sortir un tablo pos et negatif
+// $operation = new Operation();
+// $tabloOperation = $operation->showAllOperationByIdUtil($bdd,$_SESSION['id']);
+// // var_dump($tabloOperation);
+// foreach($tabloOperation as $value){
+//     if ($value->montant_operation > 0) {
+//         array_push($tabloOpTotalPos,$value->montant_operation);
+//     }
+//     if ($value->montant_operation < 0) {
+//         array_push($tabloOpTotalNeg,$value->montant_operation);
+//     }
+// }
+// $operationPos = array_sum($tabloOpTotalPos);
+// $operationNeg = array_sum($tabloOpTotalNeg);
+// // echo 'vous avez gagné '.$operationPos.'€ et dépensé '.$operationNeg.'€';
+// // j'arrive à sortir un tablo pos et negatif
 
 
-// gérer la date des operations 
-$tabloCatGlobal = [];
-$tabloCatUtil = [];
-if (isset($_POST['date_operation']) && !empty($_POST['date_operation'])) {
+// // gérer la date des operations 
+// $tabloCatGlobal = [];
+// $tabloCatUtil = [];
+// if (isset($_POST['date_operation']) && !empty($_POST['date_operation'])) {
 
-    $operationByDate  = $operation->showAllOperationByDate($bdd,$_POST['date_operation'],$_SESSION['id']);
-    // var_dump($operationByDate);
-    foreach($operationByDate as $value){
-        if ($value->id_categorie_global !== null) {
-            if (isset($tabloCatGlobal[$value->id_categorie_global])) {
-                $tabloCatGlobal[$value->id_categorie_global] += $value->montant_operation;
-            }else {
-                $tabloCatGlobal[$value->id_categorie_global] = $value->montant_operation;
-            }
-        // }elseif ($value->id_categorie_utilisateur !== null) {
-        //     ($tabloCatUtil, [$value->id_categorie_utilisateur =>$value->montant_operation]);
-        }
+//     $operationByDate  = $operation->showAllOperationByDate($bdd,$_POST['date_operation'],$_SESSION['id']);
+//     // var_dump($operationByDate);
+//     foreach($operationByDate as $value){
+//         if ($value->id_categorie_global !== null) {
+//             if (isset($tabloCatGlobal[$value->id_categorie_global])) {
+//                 $tabloCatGlobal[$value->id_categorie_global] += $value->montant_operation;
+//             }else {
+//                 $tabloCatGlobal[$value->id_categorie_global] = $value->montant_operation;
+//             }
+//         // }elseif ($value->id_categorie_utilisateur !== null) {
+//         //     ($tabloCatUtil, [$value->id_categorie_utilisateur =>$value->montant_operation]);
+//         }
     
-    }   
-}
-echo '<br>';
-var_dump($tabloCatGlobal);
-echo '<br>';
+//     }   
+// }
+// echo '<br>';
+// var_dump($tabloCatGlobal);
+// echo '<br>';
 
-///////////////////////////////////// EN PAUSE //////////////////////////////////
+// ///////////////////////////////////// EN PAUSE //////////////////////////////////
 
-// créer un tableau vide pour stocker reponse prevision
-$tabloprevisionGlobal = [];
+// // créer un tableau vide pour stocker reponse prevision
+// $tabloprevisionGlobal = [];
 
-// model cat global
-if (isset($_POST['prevision']) && !empty($_POST['prevision'])) {
+// // model cat global
+// if (isset($_POST['prevision']) && !empty($_POST['prevision'])) {
 
-        $previsionUtilSelect = $prevision->showprevision($bdd,$_POST['prevision']);
-        $avoir = new Avoir();
-        $tabloAvoir = $avoir->showAllAvoirByprevision($bdd,$_POST['prevision']);
-        foreach($tabloAvoir as $value){
-            if ($value->id_categorie_global !== null) {
-                if (isset($tabloprevisionGlobal[$value->id_categorie_global])) {
-                    $tabloprevisionGlobal[$value->id_categorie_global] += $value->budget;
-                }else {
-                    $tabloprevisionGlobal[$value->id_categorie_global] = $value->budget;
-                }
-            }
-        }
-}
-var_dump($tabloprevisionGlobal);
+//         $previsionUtilSelect = $prevision->showprevision($bdd,$_POST['prevision']);
+//         $avoir = new Avoir();
+//         $tabloAvoir = $avoir->showAllAvoirByprevision($bdd,$_POST['prevision']);
+//         foreach($tabloAvoir as $value){
+//             if ($value->id_categorie_global !== null) {
+//                 if (isset($tabloprevisionGlobal[$value->id_categorie_global])) {
+//                     $tabloprevisionGlobal[$value->id_categorie_global] += $value->budget;
+//                 }else {
+//                     $tabloprevisionGlobal[$value->id_categorie_global] = $value->budget;
+//                 }
+//             }
+//         }
+// }
+// var_dump($tabloprevisionGlobal);
 // var_dump($tabloCatGlobal);
 // var_dump($tabloCatUtil);
 // var_dump($tabloOpTotalPos);
@@ -120,7 +143,7 @@ var_dump($tabloprevisionGlobal);
 
 // lancer le script en fonction des tableaux reçu
 // echo '<script>', 'diagramme(["loisir","plaisir","restaurant","banque"],[140,500,800,300]);', '</script>';
-echo '<script>', 'diagramme(["loisir:Dépense","Loisir:Prevision"],[80,500]);', '</script>';
+echo '<script>', 'diagramme('.$tabloNomJson.','.$tabloMontantJson.');', '</script>';
 
 
 
